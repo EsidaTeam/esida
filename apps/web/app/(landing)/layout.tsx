c292bdba9e9f393473428c74c8a82f9f5257d141
Damir Modyarov <otomir@yandex.ru>
2022-11-23 20:42:11 +0300

    Add header background blur

--- apps/web/app/(landing)/layout.tsx
+++ apps/web/app/(landing)/layout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout({children}: {
 }) {
     return (
         <>
-            <header className="px-16 py-6 flex justify-between items-center">
+            <header className="px-16 py-6 flex justify-between items-center backdrop-blur sticky top-0 left-0 right-0">
                 <Link href="/" className="font-bold text-3xl flex flex-row gap-4 w-fit">
                     <Image alt="esida logo" src="/assets/images/logo.png" width={36} height={36} quality={100} />
                     <span className="sm:block hidden">esida</span>
